21d8180614c976326db2741f674d47d4b0263480
asantesylvesterkwame <intelligentson17@gmail.com>
2024-10-07 04:11:25 +0000

    designed ChatPage

---
+++ frontend/src/components/NewPrompt/NewPrompt.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from "react";
+import "./NewPrompt.css";
+import attachment from "../../../Public/images/attachment.png";
+import arrow from "../../../Public/images/arrow.png";
+
+const NewPrompt = () => {
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+  return (
+    <>
+      <div ref={endRef} className="endChat"></div>
+      <form className="newForm">
+        <label htmlFor="file" className="newFormLabel">
+          <img src={attachment} alt="" className="newFormButtonImage" />
+        </label>
+        <input type="file" id="file" hidden multiple={false} />
+        <input
+          type="text"
+          placeholder="Ask me anything..."
+          className="newFormInput"
+        />
+        <button className="newFormButton">
+          <img src={arrow} alt="" className="newFormButtonImage" />
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default NewPrompt;
--- frontend/src/pages/Chat/Chat.jsx
+++ frontend/src/pages/Chat/Chat.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./Chat.css";
+import NewPrompt from "../../components/NewPrompt/NewPrompt";
 
 const Chat = () => {
-  return <div>Chat</div>;
+
+  return (
+    <div className="chatMain">
+      <div className="chatWrapper">
+        <div className="chat">
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+          </div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>{" "}
+          <div className="message">Test Answer from AI</div>
+          <div className="message user">Test prompt from User</div>
+          <NewPrompt />
+         
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
